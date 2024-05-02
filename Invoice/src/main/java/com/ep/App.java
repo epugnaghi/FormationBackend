c0bc42adceeb15e49eb5e031351361fa72375275
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-05-02 16:12:50 +0200

    update
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

--- Invoice/src/main/java/com/ep/App.java
+++ Invoice/src/main/java/com/ep/App.java
@@ -2,17 +2,28 @@ package com.ep;
 
 import com.ep.controller.IControllerInvoice;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan(basePackages = {"com.ep.controller.console", "com.ep.service.prefix", "com.ep.repository.memory"})
+@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class App
 {
     public static void main(String[] args)
     {
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         IControllerInvoice invoiceController = context.getBean(IControllerInvoice.class);
 
         invoiceController.createInvoice();
     }
+
+    /*
+    @Bean
+    public IServiceInvoice configServiceInvoice()
+    {
+        return new ServiceInvoicePrefix();
+    }
+    */
 }
