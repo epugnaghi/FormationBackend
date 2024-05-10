581eebb9c83242c948c4e91c01aa8d9ca94b2a86
epugnaghi <etienne.pugnaghi@gmail.com>
2024-05-10 14:08:39 +0200

    update
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

---
+++ activeMQ/src/main/java/com/activeMQ/ActiveMqApplication.java
@@ -0,0 +1,15 @@
+package com.activeMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ActiveMqApplication.class, args);
+	}
+
+}
