bbf1798969b2454b93c9931bf7c240ab4149abec
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-05-13 16:46:19 +0200

    update
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

--- Invoice/src/main/java/com/ep/App.java
+++ Invoice/src/main/java/com/ep/App.java
@@ -4,11 +4,10 @@ import com.ep.controller.IControllerInvoice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
-@ImportResource("classpath:applicationContext.xml")
+//@ImportResource("classpath:applicationContext.xml")
 public class App
 {
     public static void main(String[] args)
