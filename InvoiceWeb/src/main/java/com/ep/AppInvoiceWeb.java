29a19a68f49759420d2d1f2043f2099e106d09ff
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-05-14 12:25:43 +0200

    update
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

--- InvoiceWeb/src/main/java/com/ep/AppInvoiceWeb.java
+++ InvoiceWeb/src/main/java/com/ep/AppInvoiceWeb.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppInvoiceWeb
 {
+    /*dans rep ep car il va parser à la recherche de bean dans le rep ep du projet invoice*/
     public static void main(String[] args)
     {
         SpringApplication.run(AppInvoiceWeb.class, args);
