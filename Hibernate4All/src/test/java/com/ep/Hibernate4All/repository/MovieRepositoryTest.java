ef219803a8fbfc92f3a582f1f44bd73183a6f16b
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-06-14 11:14:02 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
    
    # Conflicts:
    #	Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java

--- Hibernate4All/src/test/java/com/ep/Hibernate4All/repository/MovieRepositoryTest.java
+++ Hibernate4All/src/test/java/com/ep/Hibernate4All/repository/MovieRepositoryTest.java
@@ -4,6 +4,8 @@ import com.ep.Hibernate4All.config.PersistenceConfig;
 import com.ep.Hibernate4All.domain.Movie;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,15 +15,20 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(classes = {PersistenceConfig.class})
 public class MovieRepositoryTest
 {
+
     @Autowired
     private MovieRepository repository;
 
     @Test
     public void save_casNominal()
     {
+        System.out.println("MovieRepositoryTest/save_casNominal");
+
         Movie movie = new Movie();
         movie.setName("inception");
 
         repository.persist(movie);
+
+        System.out.println("MovieRepositoryTest/save_casNominal");
     }
 }
