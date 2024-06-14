3f122f9a22a38ff72a0d5c1e8841db0a6ada439c
epugnaghi <etienne.pugnaghi@gmail.com>
2024-06-14 09:06:02 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

---
+++ Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java
@@ -0,0 +1,18 @@
+package com.ep.Hibernate4All.repository;
+
+import com.ep.Hibernate4All.domain.Movie;
+
+import java.util.List;
+
+public class MovieRepository
+{
+    public void persist(Movie movie)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Movie> getAll()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
