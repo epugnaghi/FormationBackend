f3d36d8a96d190eec3a26258d6dbde4e087cd6c7
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-06-14 10:02:49 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>

--- Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java
+++ Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java
@@ -1,14 +1,23 @@
 package com.ep.Hibernate4All.repository;
 
 import com.ep.Hibernate4All.domain.Movie;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class MovieRepository
 {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
     public void persist(Movie movie)
     {
-        throw new UnsupportedOperationException();
+        entityManager.persist(movie);
     }
 
     public List<Movie> getAll()
---
+++ Hibernate4All/src/test/java/com/ep/Hibernate4All/repository/MovieRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.ep.Hibernate4All.repository;
+
+import com.ep.Hibernate4All.config.PersistenceConfig;
+import com.ep.Hibernate4All.domain.Movie;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {PersistenceConfig.class})
+public class MovieRepositoryTest
+{
+    @Autowired
+    private MovieRepository repository;
+
+    @Test
+    public void save_casNominal()
+    {
+        Movie movie = new Movie();
+        movie.setName("inception");
+
+        repository.persist(movie);
+    }
+}
