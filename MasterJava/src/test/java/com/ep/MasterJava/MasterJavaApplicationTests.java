99e1fdf4dc513265bc01584d115b299adc216a5b
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-06-28 10:31:51 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
    
    # Conflicts:
    #	Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java

--- MasterJava/src/test/java/com/ep/MasterJava/MasterJavaApplicationTests.java
+++ MasterJava/src/test/java/com/ep/MasterJava/MasterJavaApplicationTests.java
@@ -1,9 +1,8 @@
 package com.ep.MasterJava;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MasterJavaApplicationTests
 {
     @Test
