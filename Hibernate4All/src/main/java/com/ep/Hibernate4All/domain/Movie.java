731599fb9bc27d27cc07aee18eabdc947ba85200
Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
2024-07-02 15:45:47 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Etienne Pugnaghi <etienne.pugnaghi@gmail.com>
    
    # Conflicts:
    #	Hibernate4All/src/main/java/com/ep/Hibernate4All/repository/MovieRepository.java

--- Hibernate4All/src/main/java/com/ep/Hibernate4All/domain/Movie.java
+++ Hibernate4All/src/main/java/com/ep/Hibernate4All/domain/Movie.java
@@ -5,8 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//@Data is a shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, @Setter on all non-final fields, and @RequiredArgsConstructor
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Movie
 {
     @Id
@@ -14,38 +21,4 @@ public class Movie
     private Long id;
     private String name;
     private String description;
-
-    public Movie()
-    {
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
 }
